64f0a7bf7b39e81f44d38aa8bed60f4d103989fc
Pufferion <31212860+Pufferion@users.noreply.github.com>
2021-02-19 16:20:28 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -3,12 +3,12 @@ module.exports = {
     description: "displays bot info",
     execute(msg, bot, Discord, prefix) {
         const embed = new Discord.MessageEmbed()
-        .setColor('#a913cf')
-        .addFields(
-            {name: "Uptime", value: process.uptime().toString()},
-            {name: "Prefix:", value: prefix}
-        )
-        .setFooter("omg what")
+            .setColor('#a913cf')
+            .addFields(
+                {name: "Uptime", value: process.uptime().toString()},
+                {name: "Prefix:", value: prefix}
+            )
+            .setFooter("omg what")
         msg.channel.send(embed);
     }
 }
