26075fb993abc9b9cc58d4bb7d227ba196017b4e
Pufferion <31212860+Pufferion@users.noreply.github.com>
2021-03-05 17:04:39 +0200

    added perm check and addded reason for kick

--- commands/kick.js
+++ commands/kick.js
@@ -4,9 +4,15 @@ module.exports = {
     execute(msg, args) {
         const mention = msg.mentions.users.first();
         if (msg.member.hasPermission('KICK_MEMBERS')) {
-            const target = msg.guild.members.cache.get(mention.id);
-            target.kick();
-            mention.send("oops you've been kicked")
+            try {
+                const target = msg.guild.members.cache.get(mention.id);
+                target.kick();
+                mention.send("Oops, you've been kicked because: " + args[1] + ".");
+            } catch {
+                msg.channel.send(":flushed: I don't have the right permissions..");
+            }
+        }else {
+            msg.channel.send(":flushed: You don't have permission..")
         }   
     }
 }
